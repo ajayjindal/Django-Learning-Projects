6f29c755ae360b13ab012abef88ed82b16af1a25
Ajay Jindal <ajay96jindal@gmail.com>
2018-09-11 12:24:42 +0530

    database changed to postgres

--- protfolio/jobs/models.py
+++ protfolio/jobs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+	"""docstring for Jobs"""
+	image = models.ImageField(upload_to='')
+	summary = models.CharField(max_length=200)
+		
